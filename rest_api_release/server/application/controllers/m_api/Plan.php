76ea9e92f4920824b83f23d74a87018afaceb91f
sampurn0 <sampurno.nugroho@gmail.com>
2020-05-06 18:45:23 +0700

    A5aaa

--- rest_api_release/server/application/controllers/m_api/Plan.php
+++ rest_api_release/server/application/controllers/m_api/Plan.php
@@ -306,8 +306,6 @@ class Plan extends REST_Controller {
 			$list[$key]['s1k'] = $r['pcs_1000'];
 			$list[$key]['coin'] = $r['pcs_coin'];
 			$list[$key]['bag_seal'] = $r['bag_seal'];
-			$list[$key]['bag_seal'] = $r['bag_seal'];
-			$list[$key]['bag_seal'] = $r['bag_seal'];
 			$list[$key]['bag_no'] = $r['bag_no'];
 			
 			
