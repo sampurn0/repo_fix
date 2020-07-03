3ebc081a34c6e759796f82cfd6ceec3c59f439ba
sampurn0 <sampurno.nugroho@gmail.com>
2020-07-03 16:27:59 +0700

    UPDATE REKON

--- monitor/application/controllers/Sub_Kategori.php
+++ monitor/application/controllers/Sub_Kategori.php
@@ -2,7 +2,7 @@
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Sub_Kategori extends CI_Controller {
+class Sub_kategori extends CI_Controller {
     var $data = array();
 
     public function __construct() {
